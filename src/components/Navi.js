ed6e17594d406bb03a26fd1ae2a3598232a2568a
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-12-02 19:50:16 +0900

    Navi

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -10,10 +10,40 @@ function navi() {
           <img id="nav-logo" alt='instagram-logo' src={require('../assets/Instagram_logo.png')} />
         </Link>
       </div>
-      <div className="home">
+      <div className="nav-list">
         <li>
           <Link to={"/"}>
             홈
+          </Link>
+        </li>
+        <li>
+          <Link to={"/"}>
+            검색
+          </Link>
+        </li>
+        <li>
+          <Link to={"/"}>
+            탐색 탭
+          </Link>
+        </li>
+        <li>
+          <Link to={"/"}>
+            메시지
+          </Link>
+        </li>
+        <li>
+          <Link to={"/"}>
+            알림
+          </Link>
+        </li>
+        <li>
+          <Link to={"/"}>
+            만들기
+          </Link>
+        </li>
+        <li>
+          <Link to={"/"}>
+            프로필
           </Link>
         </li>
       </div>
