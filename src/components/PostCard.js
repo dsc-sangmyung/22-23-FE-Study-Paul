cbdff5774595cf84070e3993be2c059e95638cea
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2022-12-07 12:00:00 +0900

    Synchronization

---
+++ src/components/PostCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './PostCard.css'
+
+import { Link } from "react-router-dom";
+
+function PostCard() {
+  return (
+    <div className="post-box">
+      hello
+    </div>
+  )
+}
+
+export default PostCard
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,16 @@
 import React from "react"
 import './MainPage.css'
+
 import Navi from '../components/Navi'
+import PostCard from '../components/PostCard'
 
 function mainpage() {
   return (
     <div>
       <Navi />
+      <div>
+        <PostCard />
+      </div>
     </div>
   )
 }
