5ffa9c8314d9c8277f47ae73fae39e0214aec3f5
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-01-03 17:50:51 +0900

    Login Page - id, pw input box

--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -5,9 +5,15 @@ import { Link } from "react-router-dom";
 function LoginBox() {
   return (
     <div className="box">
-      <Link to={"/"}>
-        <img id="login-logo" alt="instagram-logo" src={require('../assets/Instagram_logo.png')} />
-      </Link>
+      <div className="login-logo">
+        <Link to={"/"}>
+          <img id="login-logo-img" alt="instagram-logo" src={require('../assets/Instagram_logo.png')} />
+        </Link>
+      </div>
+      <div className="input-box">
+        <input id="id-input-box" placeholder="전화번호, 사용자 이름 또는 이메일" />
+        <input id="pw-input-box" placeholder="비밀번호"/>
+      </div>
     </div>
   )
 }
