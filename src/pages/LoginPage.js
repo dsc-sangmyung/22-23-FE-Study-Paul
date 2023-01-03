55f613ec52049e6a4678c7fdd1c096811108ba26
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-01-03 21:07:48 +0900

    login page complete

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -10,6 +10,19 @@ function LoginPage() {
       <div className="login-tab">
         <LoginBox />
       </div>
+      <div className="sign-up">
+        <div id="no-account">
+          계정이 없으신가요?
+        </div>
+        <div id="sign-up-link">
+          <Link to={"/"}>
+            가입하기
+          </Link>
+        </div>
+      </div>
+      <div className="download-guide">
+        앱을 다운로드하세요.
+      </div>
     </div>
   )
 }
